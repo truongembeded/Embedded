baaad584dd8d8cff1d1e019e5f54534b120b4a47
Truong <nguyenvantruong2010cdt@gmail.lcom>
2023-05-19 21:48:58 +0700

    BT bit 1

--- BaiTapC/ThaoTacBit/main.c
+++ BaiTapC/ThaoTacBit/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
+uint8_t PORTA = 0b00000100;
+
 void ReadByte(uint8_t byte) {
    for (int i = 0; i < 8; i++) {
       uint8_t bit = (byte >>(7 - i)) & 1;
@@ -9,10 +11,57 @@ void ReadByte(uint8_t byte) {
    printf("\n");
 }
 
+typedef enum{
+   PIN0,
+   PIN1,
+   PIN2,
+   PIN3,
+   PIN4,
+   PIN5,
+   PIN6,
+   PIN7
+}pins;
+
+typedef enum{
+   LOW,
+   HIGH
+}digital;
+
+void PinHigh(pins pin){
+   PORTA = PORTA | ( 1 << 7 - pin );
+
+}
+void PinLow(pins pin){
+   PORTA = PORTA & ~( 1 << 7 - pin );
+}
+
+void DigitalWrite(pins pin, digital result){
+   switch (result) {
+        case LOW:
+            PinLow(pin);
+            break;
+        case HIGH:
+            PinHigh(pin);
+            break;
+        default:
+            printf("ERROR\n");
+            break;
+    }
+}
+
+
 int main() {
-   uint8_t a = 0b01000100;
+   //uint8_t a =10;
+   
+   
+   DigitalWrite(PIN5, LOW);
+   DigitalWrite(PIN5, HIGH);
+   DigitalWrite(PIN5, LOW);
+   DigitalWrite(PIN5, HIGH);
+   DigitalWrite(PIN5, LOW);
    printf("0b");
-   ReadByte(a);
+   ReadByte(PORTA);
+
    return 0;
 }
 
